4ba8216fee5905b7f2927aad575f018171e32371
WillSchwieder <wschwied@chicagobooth.edu>
2022-03-01 04:15:48 +0000

    Added the new navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,35 @@ class UsersController < ApplicationController
   end
 
   def sign_out
-    render({ :template => "user_templates/sign_out.html.erb" })
+    reset_session
+
+    redirect_to("/", { :notice => "See ya later!" })
   end
 
   def sign_in
     render({ :template => "user_templates/sign_in.html.erb" })
   end
+
+  def verify_credentials
+    redirect_to("/users/#{user.username}", { :notice => "Welcome, " + user.username + "!" })
+  end
+
+  def authenticate
+    username = params.fetch("input_username")
+    password = params.fetch("input_password")
+
+    user = User.where({ :username => username }).at(0)
+
+    if user == nil
+      redirect_to("/user_sign_in", { :alert => "No one by that name here" })
+    else
+      if user.authenticate(password)
+        session.store(:user_id, user.id)
+        redirect_to("/", { :notice => "Welcome Back " + user.username + "!" })
+      else
+        redirect_to("/user_sign_in", {:alert => "Incorrect password"})
+      end
+    end
+  end
+
 end
